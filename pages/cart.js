78db90013712670585b3673893107fb52c50bacf
Gen4ralz <acetyl-coa@windowslive.com>
2022-07-12 01:46:29 +0700

    Update-Quantity-of-Cart

--- pages/cart.js
+++ pages/cart.js
@@ -11,6 +11,10 @@ export default function CartScreen() {
   const {
     cart: { cartItems },
   } = state;
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+  };
   return (
     <Layout title="Shopping Cart">
       <h1 className="font-bold text-xl mb-10 mt-5 md:px-4">Order Details</h1>
@@ -30,13 +34,15 @@ export default function CartScreen() {
             {cartItems.map((item) => (
               <li key={item.slug} className="flex py-6 md:px-4">
                 <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    width="100%"
-                    height="100%"
-                    className="object-center"
-                  />
+                  <Link href={`/product/${item.slug}`}>
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width="100%"
+                      height="100%"
+                      className="object-center"
+                    />
+                  </Link>
                 </div>
                 <div className="ml-4 flex flex-1 flex-col">
                   <div>
@@ -49,7 +55,21 @@ export default function CartScreen() {
                     </p>
                   </div>
                   <div className="flex flex-1 items-end justify-between text-sm">
-                    <p className="text-gray-500 px-2">Qty : {item.quantity}</p>
+                    <p className="text-gray-500 px-2">
+                      Qty :{' '}
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.stock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </p>
                     <div className="flex">
                       <button
                         type="button"
