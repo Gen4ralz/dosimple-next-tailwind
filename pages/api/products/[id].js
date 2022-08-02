f9530226ba4900690ca72238511296f0118debee
Gen4ralz <acetyl-coa@windowslive.com>
2022-08-02 17:14:31 +0700

    Fix fetching product api

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,7 +1,7 @@
 import Product from '../../../models/Product';
 import db from '../../../utils/db';
 
-const handler = async (res, req) => {
+const handler = async (req, res) => {
   await db.connect();
   const product = await Product.findById(req.query.id);
   await db.disconnect();
